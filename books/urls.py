cbb30635908b314295e4020258ee0814c2a213eb
Reyna Jerezano <reyna.jerezano@gmail.com>
2026-03-10 20:13:15 -0500

    Cambiar ruta del home

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name="home"),
     path('publisher/', views.list_publisher, name='list_publisher'),
     path('add_publisher/', views.add_publisher, name='add_publisher'),
     path('edit_publisher/<int:id>/', views.edit_publisher, name='edit_publisher'),
@@ -11,7 +10,7 @@ urlpatterns = [
     path('add_author/', views.add_author, name='add_author'),
     path('edit_author/<int:id>/', views.edit_author, name='edit_author'),
     path('delete_author/<int:id>/', views.delete_author, name='delete_author'),
-    path('list', views.list_book, name='list_books'),
+    path('list/', views.list_book, name='list_books'),
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:id>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:id>/', views.delete_book, name='delete_book'),
--- project_books/urls.py
+++ project_books/urls.py
@@ -23,5 +23,6 @@ from books import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('books/', include('books.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
